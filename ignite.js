e1422dd463c36ac4a9bfc33c13ee7367c4b2749d
Brad Cypert <brad.cypert@gmail.com>
2015-03-22 17:25:23 -0500

    Template created. beginning testing.

--- ignite.js
+++ ignite.js
@@ -5,4 +5,32 @@ process.title = 'Ignite';
 //ignite --help
 //ignite scaffold
 
+// No Unit Tests :P
+
 var args = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+
+function createFile(jsonObject, path){
+  console.log(path);
+
+  for(var attributename in jsonObject){
+    if(typeof jsonObject[attributename] == "object"){
+      createFile(jsonObject[attributename], (path+"/"+attributename).replace('undefined', ''));
+    }
+    else
+      console.log("\t"+jsonObject[attributename]);
+  }
+}
+
+if(args.help){
+  console.log("Usage: \tignite [options] [arguments]",
+              "\n\nTo scaffold a new project:",
+              "\n\tignite scaffold"
+              );
+} else if (args._[0] == "scaffold" && args._[1]) {
+  templateName = args._[1];
+  templateObject = JSON.parse(fs.readFileSync('templates/'+templateName+'.json', 'utf8'));
+  //do the things here
+  //recursively create folder structure
+  createFile(templateObject.structure);
+}
