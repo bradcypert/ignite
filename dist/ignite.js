9f5bfc39b83c2f7546a1e7f20bfe93df57a43100
Austin Turnage <austin@datarats.com>
2015-04-02 15:57:08 -0400

    pulling from express now saves file

--- dist/ignite.js
+++ dist/ignite.js
@@ -4,6 +4,54 @@
 
 var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
 
+// This is a hack to simply host the template files somewhere for now
+exports.startApiServer = startApiServer;
+exports.getApi = getApi;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var request = _interopRequire(require("request"));
+
+var express = _interopRequire(require("express"));
+
+var path = _interopRequire(require("path"));
+
+var fs = _interopRequire(require("fs"));
+
+var templatePath = path.join("" + __dirname + "/../templates/");
+function startApiServer() {
+  var app = express();
+  app.use("/user", express["static"]("$[process.cwd()}/templates"));
+  app.listen(process.env.PORT || 3000);
+}
+
+function getApi(user, name) {
+  var data = {
+    method: "GET",
+    url: "http://localhost:3000/" + user + "/" + name + ".json" };
+  if (user && name) {
+    startApiServer();
+    request(data).pipe(fs.createWriteStream("" + templatePath + "download-" + name + ".json"));
+    console.log("Downloaded");
+    process.exit(0);
+  } else {
+    if (!user) {
+      console.log("No Username");
+      process.exit(0);
+    }
+    if (!name) {
+      console.log("No project");
+      process.exit(0);
+    }
+  }
+}
+
+;
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
 /**
 * Get paths from template schema
 */
@@ -151,6 +199,9 @@ switch (args._[0]) {
   case "verison":
     console.log("Ignite Verison:", pkg.version);
     break;
+  case "pull":
+    getApi(args._[1], args._[2]);
+    break;
   default:
     console.log("Incorrect usage: Try ignite help for more information on how to use this tool.");
     break;
@@ -182,9 +233,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var fs = _interopRequire(require("fs"));
-
 var path = _interopRequire(require("path"));
+
+var fs = _interopRequire(require("fs"));
 
 var localTemplates = "" + __dirname + "/../templates";
 function logError(code, message) {
