7f1e31f83d8f8365f3041a488d912abf0d18a2aa
Austin Turnage <austin@datarats.com>
2015-04-03 10:02:39 -0400

    gulp to compile lib in order

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var insert = require('gulp-insert');
 
 
 gulp.task('default', function () {
-    return gulp.src('lib/*.js')
+    return gulp.src([
+                    'lib/fileHelpers.js',
+                    'lib/ghUtils.js',
+                    'lib/utils.js',
+                    'lib/ignite.js'])
         .pipe(babel())
         .pipe(concat('ignite.js'))
         .pipe(insert.prepend('#!/usr/bin/env node\n\n'))
